0fdde863d501c8885b9257e10dc079ab7e8e15a2
Thanvitha Kariveda <thanvithakariveda06@gmail.com>
2026-03-06 20:25:29 +0530

    Add files via upload

---
+++ asynchronous.js
@@ -0,0 +1,24 @@
+//timer function
+
+console.log("first")
+     setTimeout(()=>{
+        console.log("task completed")
+    },3000);
+console.log("second")
+console.log("three")
+
+
+
+//setInterval
+setInterval(()=>{
+    console.log("timer called")
+},2000)
+
+
+console.log("i am thanvitha")
+setTimeout(()=>
+{
+    console.log("i am tired to type")
+},5000);
+console.log("Now i typed")
+console.log("The output is ready")
